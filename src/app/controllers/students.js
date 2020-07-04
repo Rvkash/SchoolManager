37ea3fb9b53005f768edefee0f7d8dbf3218bfb3
Rukash <rafael.ieel@hotmail.com>
2020-07-04 11:54:56 -0300

    controllers update

---
+++ src/app/controllers/students.js
@@ -0,0 +1,83 @@
+/* eslint-disable */
+const Student = require('../models/Student')
+const { age, date } = require('../../lib/utils')
+// req.query = id=?
+// req.body = corpo
+// req.params = /:id/members
+
+// show //create //edit // put
+
+module.exports = {
+  index (req, res) {
+    Student.all(function (students) {
+      return res.render('students/index', { students })
+    })
+  },
+
+  create (req, res) {
+    
+    Student.teachersSelectOptions(function (options) {
+      return res.render('students/create', { teacherOptions: options })
+    })
+  },
+
+  post (req, res) {
+    // req.query  // req.body
+
+    const keys = Object.keys(req.body)
+    // req.body  {avatar_url: "http://avatarulr.com" "name": 123 etc...}
+    for (key of keys) {
+    // req.body.key == ""
+      if (req.body[key] == '') {
+        return res.send('Please, fill all fields ')
+      }
+    }
+
+    Student.create(req.body, function (student) {
+      return res.redirect(`/students/${student.id}`)
+    })
+  },
+
+  show (req, res) {
+    Student.find(req.params.id, function (student) {
+      if (!student) return res.send('student not found')
+
+      student.birth = date(student.birth).birthDay
+
+      return res.render('students/show', { student })
+    })
+  },
+
+  edit (req, res) {
+    Student.find(req.params.id, function (student) {
+      if (!student) return res.send('student not found')
+
+      student.birth = date(student.birth).iso
+
+      Student.teachersSelectOptions(function (options) {
+        return res.render('students/edit', { student, teacherOptions: options })
+      })
+    })
+  },
+
+  put (req, res) {
+    const keys = Object.keys(req.body)
+    // req.body  {avatar_url: "http://avatarulr.com" "name": 123 etc...}
+    for (key of keys) {
+    // req.body.key == ""
+      if (req.body[key] == '') {
+        return res.send('Please, fill all fields ')
+      }
+    }
+
+    Student.update(req.body, function () {
+      return res.redirect(`students/${req.body.id}`)
+    })
+  },
+
+  delete (req, res) {
+    Student.delete(req.body.id, function () {
+      return res.redirect('/students')
+    })
+  }
+}
---
+++ src/app/controllers/teachers.js
@@ -0,0 +1,87 @@
+/* eslint-disable */
+const Teacher = require('../models/Teachers')
+const { age, date } = require('../../lib/utils')
+// req.query = id=?
+// req.body = corpo
+// req.params = /:id/members
+
+// show //create //edit // put
+
+module.exports = {
+  index (req, res) {
+
+    Teacher.all(function(teachers) {
+      return res.render('teachers/index', { teachers })
+    })
+   },
+
+  create(req, res) {
+
+      return res.render('teachers/create')
+      
+  },
+  
+  post (req, res) {
+    // req.query  // req.body
+
+    const keys = Object.keys(req.body)
+    // req.body  {avatar_url: "http://avatarulr.com" "name": 123 etc...}
+    for (key of keys) {           
+    // req.body.key == ""
+      if (req.body[key] == '') {   
+        return res.send('Please, fill all fields ')
+      }
+    }
+
+   Teacher.create(req.body, function(teacher){
+    return res.redirect(`/teachers/${teacher.id}`)
+   })
+   
+  },
+
+  show (req, res) {
+    Teacher.find(req.params.id, function(teacher){
+      if(!teacher) return res.send("teacher not found")
+
+      teacher.age = age(teacher.birth)
+      teacher.services = teacher.services.split(",")
+
+      teacher.created_at = date(teacher.created_at).format
+
+      return res.render('teachers/show', { teacher })
+    })
+  },
+
+  edit (req, res) {
+    Teacher.find(req.params.id, function(teacher){
+      if(!teacher) return res.send("teacher not found")
+
+      teacher.birth = date(teacher.birth).iso
+
+      return res.render('teachers/edit', { teacher })
+
+    })
+  },
+
+  put (req, res) {
+    const keys = Object.keys(req.body)
+    // req.body  {avatar_url: "http://avatarulr.com" "name": 123 etc...}
+    for (key of keys) {
+    // req.body.key == ""
+      if (req.body[key] == '') {
+        return res.send('Please, fill all fields ')
+      }
+    }
+
+    Teacher.update(req.body, function(){
+      return res.redirect(`teachers/${req.body.id}`)
+    })
+
+  },
+
+  delete (req, res) {
+    Teacher.delete(req.body.id, function(){
+      return res.redirect(`/teachers`)
+    })
+  }
+}
