940e99d77ceba35c30ac4a965b0d5176e9a9501f
Rukash <rafael.ieel@hotmail.com>
2020-07-06 00:27:15 -0300

    add: paginate routes and logic

--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -8,21 +8,33 @@ const { age, date } = require('../../lib/utils')
 
 module.exports = {
   index (req, res) {
-    const { filter} = req.query
+    let { filter, page, limit} = req.query
 
-    if (filter) {
-      Teacher.findBy(filter, function(teachers) {
-        return res.render('teachers/index', {teachers})
-      })
+    page = page || 1
+    limit = limit || 2 
+    let offset = limit * (page - 1)
 
-    } else {
-    Teacher.all(function(teachers) {
-      return res.render('teachers/index', { teachers })
-    })
-   }
+    const params = {
+      filter,
+      page,
+      limit,
+      offset,
+      callback(teachers) {
+        
+    const pagination = {
+      total: Math.ceil(teachers[0].total / limit),
+      page
+    }
+
+    return res.render("teachers/index", { teachers, pagination, filter })
+    
+    }
+  }
+
+    Teacher.paginate(params)
+    
   },
-    
-    
+
   create(req, res) {
 
       return res.render('teachers/create')
